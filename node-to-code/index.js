487bc8fbf27d0e2a822de6db33a2882526b3801c
Anastasia <a.plata@mcinnovations.com.gr>
2022-12-02 13:58:57 +0200

    node.js

---
+++ node-to-code/index.js
@@ -0,0 +1,6 @@
+/* var fs= require("fs");
+fs.copyFileSync("file.txt","file1.txt");
+var superheroes=require("superheroes");
+var myHeroesNames=superheroes.random();
+console.log("myHeroesNames");*/
+
