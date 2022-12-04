e142943047a14cd698168f06bb65820520b49e2e
Anastasia Plata <a.plata@mcinnivations.com.gr>
2022-12-04 19:14:29 +0200

    sxolia kai parathrhseis gia post , on , express , nodemon , http

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,5 +1,9 @@
 
 const express= require("express");
+/*einai to body tou node.js (server) kai 
+zhtaei na diaxeiristei to http post request apo 
+ta data pou uparxoun sto body toy server ki na ta 
+apokodikopoisei */
 const bodyParser= require("body-parser");
 
 const app=express();
@@ -10,13 +14,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req,res){
    // res.send("<h1>Hello Anastasia!</h1>");
    res.sendFile(__dirname + "/calculator.html");
+   //gurnaei to arxeio tou html 
 });
 app.post("/", function(req,res){
-
+//zhtaei ta sugkekrimena data apo ton server
+// to post -> postarei ston browser  
     var num1= Number(req.body.num1);
     var num2= Number(req.body.num2);
     var result= num1+num2 ;
-
+// emfanizei ston browser 
     res.send("Το αποτέλεσμα της πράξης είναι "+ result);
  });
-app.listen(3000);
+app.listen(3000); //localhost 
--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -5,17 +5,25 @@ const app = express();
 const https= require("https");
 
 app.get("/", function(req,res){
-
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=2b09402f4a28c9a0860d823f88979db7&units=metric ";
-
+/* ζητώ να γίνει η διαδικασία request και response από τον δικό μου σερβερ 
+στο browser και από τον δικό μου μεσω API σε άλλον σερβερ*/
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=2b09402f4a28c9a0860d823f88979db7&units=metric";
+/* και ζητώ response (DATA) από την ιστοδελίδα με συγκεκριμένο url 
+στον δικό μου σερβερ  */
     https.get(url, function(res) {
-
+/*οn-> addlistener function στην express όπου θέλουμε τα data που θα γυρίσει 
+ να είναι αποκωδικοποιημένο μήνυμα από text  σε java script object  */
 res.on("data", function(data){
-    console.log(data);
-
-})
-})
-    res.send();
-})
+    const weatherData=JSON.parse(data);
+    console.log(weatherData); 
+/* στο terminal θα δω τι επιστρέφεται από data στον δικό μου σερβερ */
+});
+});
+   res.send("ok");
+   /* μήνυμα που εμφανίζεται στο localhost της ιστοσελίδας μου
+   για να εμφανιστεί κάποιο data στην ιστοσελίδα χρειάζομαι html αρχείο που 
+   διαβάζει τον κώδικα με το τι θέλω να εμφανίσω και τι λειτουργικότητα θέλω να 
+   έχει  */ 
+});
 
 app.listen(3000);
